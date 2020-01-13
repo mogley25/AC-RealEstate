03dadc3cbfd3d4e419fa3cd5c5c814728fc3f232
mogley25 <43046761+mogley25@users.noreply.github.com>
2020-01-13 16:09:41 -0500

    Updated RealEstate.py
    The program now detects invalid address entries and will not open your browser to the default search url. The program will now tell you that the address is invalid and will ask for the address again.

--- RealEstate.py
+++ RealEstate.py
@@ -1,13 +1,30 @@
 import webbrowser
 import mechanize
-br = mechanize.Browser()
-br.set_handle_robots(False)
-br.addheaders = [('User-agent','Firefox')]
-br.open('http://www2.county.allegheny.pa.us/RealEstate/search.aspx')
-br.select_form(nr=0)
-HouseNum = int(input("Enter House number: "))
-StreetName = input("Enter Street Name: ")
-br.form['txtStreetNum'] = str(HouseNum)
-br.form['txtStreetName'] = str(StreetName)
-sub = br.submit()
-webbrowser.open(sub.geturl())
+ans = True
+validAddress = True
+while ans == True and validAddress == True:
+    br = mechanize.Browser()
+    br.set_handle_robots(False)
+    br.addheaders = [('User-agent','Firefox')]
+    br.open('http://www2.county.allegheny.pa.us/RealEstate/search.aspx')
+    br.select_form(nr=0)
+    HouseNum = int(input("Enter House number: "))
+    StreetName = input("Enter Street Name: ")
+    br.form['txtStreetNum'] = str(HouseNum)
+    br.form['txtStreetName'] = str(StreetName)
+    sub = br.submit()
+    if sub.geturl() == 'http://www2.county.allegheny.pa.us/RealEstate/search.aspx':
+        validAddress == False
+        print("Address does not exist. Try again.")
+        continue
+
+    else:
+        validAddress == True
+
+    webbrowser.open(sub.geturl())
+
+    question = input("Would you like to run again? y/n ")
+    if question == 'y':
+        ans = True
+    else:
+        ans = False
